665a6986c8ef927d3ed6dde2db94e781fe3cdc3e
waramavictor <victorkinyua16@gmail.com>
2024-05-09 21:43:03 +0300

    state-events-lab

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [details, setDetails] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onTaskFormSubmit({ text: details, category });
+    setDetails("");
+    setCategory("");
+  };
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          type="text"
+          name="text"
+          value={details}
+          onChange={(event) => setDetails(event.target.value)}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select
+          name="category"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
